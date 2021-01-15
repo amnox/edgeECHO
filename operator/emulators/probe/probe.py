8571ce80edaa6c9a6b14618761e77cb94eada998
Aman Khalid <aman.khalid@slu.edu>
2021-01-14 18:45:43 -0600

    Probe and Streaming

--- operator/emulators/probe/probe.py
+++ operator/emulators/probe/probe.py
@@ -1,56 +1,67 @@
 import socket 
-  
-# import thread module 
-from _thread import *
-import threading 
-  
-print_lock = threading.Lock() 
-  
-# thread function 
-def threaded(c): 
-    while True: 
-  
-        # data received from client 
-        data = c.recv(1024) 
-        if not data: 
-            print('Bye') 
-            break
-  
-        # reverse the given string from client 
-        data = data[::-1] 
-  
-        # send back reversed string to client 
-        c.send(data) 
-  
-    # connection closed 
-    c.close() 
-  
-  
+import json, enum
+
+message_count = 100
+probe_ip = "127.0.0.1" 
+probe_port = 12345
+
+class ProbeMessage(enum.Enum):
+   data = 1
+   terminate = 2
+   connection = 3
+
+socket_message = {
+        'sender':'stream',
+        'type':None,
+        'data':None
+    }
+def create_message(sender, m_data, m_type):
+    data = socket_message
+    data['sender'] = sender
+    data['data'] = m_data
+    data['type'] = m_type
+    data = json.dumps(data)+';'
+    return data.encode('ascii')
+def form_message(data):
+    print("".join(data))
+    return json.loads("".join(data))
+def wait_for_feedback(c):
+    data = []
+    byte = ''
+    while True:
+        
+        byte = c.recv(1).decode('ascii')
+        if(byte==";"):
+            data = form_message(data)
+            if(data['type']==3):
+                print("Resuming Stream")
+                break
+            data = []
+            continue
+        data.append(byte ) 
 def Main(): 
-    host = "" 
-  
-    # reverse a port on your computer 
-    # in our case it is 12345 but it 
-    # can be anything 
-    port = 12345
+    
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
-    s.bind((host, port)) 
-    print("socket binded to port", port) 
-  
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((probe_ip, probe_port)) 
+    print("socket binded to port", probe_port) 
+    
     # put the socket into listening mode 
     s.listen(5) 
     print("socket is listening") 
-  
-    # a forever loop until client wants to exit 
-    while True: 
-  
-        # establish connection with client 
-        c, addr = s.accept() 
-  
-        print('Connected to :', addr[0], ':', addr[1]) 
-  
-        # Start a new thread and return its identifier 
-        start_new_thread(threaded, (c,)) 
+    # establish connection with client 
+    c, addr = s.accept() 
+    print('Connected to :', addr[0], ':', addr[1]) 
+
+    c.send(create_message('probe','You are connected to probe',3))
+    wait_for_feedback(c)
+    for i in range(0,message_count):
+        c.send(create_message('probe','1','1'))
+        print("Sent data")
+        
+    c.send(create_message('probe',1,2))
+    print("All messages sent, terminating connection")
+
     s.close() 
   
   
